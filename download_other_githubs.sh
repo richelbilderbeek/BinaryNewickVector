607a81d62dc3fdae7e635fdf98570d3a3e1cb7d9
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-25 15:44:45 +0200

    Download Newick

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -2,6 +2,10 @@
 
 cd ..
 
+if [ ! -d Newick ]; then
+ git clone https://github.com/richelbilderbeek/Newick
+fi
+
 if [ ! -d RibiClasses ]; then
  git clone https://github.com/richelbilderbeek/RibiClasses
 fi
